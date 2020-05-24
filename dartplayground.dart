a8cdf44977da8f99e86093cfb7366e83cbb0e28c
ManoelPjr <napolesjr@gmail.com>
2020-05-23 22:18:29 -0300

    First file

---
+++ dartplayground.dart
@@ -0,0 +1,39 @@
+/* Types
+ Int
+ Double
+ String
+ Bool
+ Dynamic parecida com o tipo Variant do Delphi
+*/
+import 'dart:io';
+main(){
+int Intamount1 = 100;
+var Intamount2 = 200;
+print('intAmmount1: $Intamount1 | intAmmount2: $Intamount2 \n');
+
+double Dblamount1 = 23.50;
+var Dblamount2 = 2.235;
+print('DblAmount1: $Dblamount1 | DblAmount2: $Dblamount2 \n');
+
+String Stramount1 = 'Manoel';
+var Stramount2 = 'Junior';
+print('StrAmount1: $Stramount1 | StrAmount2: $Stramount2 \n');
+
+bool Boolamount1 = true;
+var Boolamount2 = false;
+print('BoolAmount1: $Boolamount1 | BoolAmount2: $Boolamount2 \n');
+
+dynamic Dynamount1 = 'Dart';
+dynamic Dynamount2 = false;
+print('DyAmount1: $Dynamount1 | DynAmount2: $Dynamount2 \n');
+
+  // Comentário de uma linha
+  /* 
+  Comentário de múltiplas linhas
+  Tudo aqui será ignorado
+  Pelo compilador.
+  */
+  ///Documentação no código
+  ///
+  //print('Meu nome é: $fullNa me');
+  }
