ef1a85d856c0f0ff0f38c35342fecbe206b0accb
ManoelPjr <napolesjr@gmail.com>
2020-05-23 22:58:41 -0300

    Dart Defaults

---
+++ dartplaygrounddefaults.dart
@@ -0,0 +1,18 @@
+//defaults
+main(){
+//variaveis que não são atribuidos valores ficam null
+String s1;
+bool b1;
+int i1;
+double dbl1;
+dynamic dyn1;
+
+print(s1);
+print(b1);
+print(i1);
+print(dbl1);
+print(dyn1);
+//constantes não podem ser declaradas sem valor
+const int iconst1 = 1;
+print(iconst1);
+}
