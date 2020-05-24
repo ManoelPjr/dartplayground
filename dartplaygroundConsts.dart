24a29df9c01a60a9d41d8d4f59393244c9b62648
ManoelPjr <napolesjr@gmail.com>
2020-05-23 22:58:02 -0300

    Dart Const's

---
+++ dartplaygroundConsts.dart
@@ -0,0 +1,16 @@
+//Costantes
+main(){
+const ConstNum = 0; //Int contante
+const ConstDbl = 0.0; //Double contante
+const ConstBoll = true; //Boolean constante
+const ConstString = 'Manoel Pedro'; //String constante
+
+print(ConstNum.runtimeType);
+print(ConstNum);
+print(ConstDbl.runtimeType);
+print(ConstDbl);
+print(ConstBoll.runtimeType);
+print(ConstBoll);
+print(ConstString.runtimeType);
+print(ConstString);
+}
