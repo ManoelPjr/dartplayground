a0585fb815e02af2ab25f57788b6c4e40a3f5d55
ManoelPjr <napolesjr@gmail.com>
2020-05-24 00:11:14 -0300

    Dart Type test

---
+++ dartplaygroundTypeTest.dart
@@ -0,0 +1,14 @@
+//Null Aware Operators
+//(?.) , (??) , (??=)
+
+//Type test
+main(){
+//var x = 100;
+var x = 10.55;
+if(x is int){
+  print('X is an Integer');
+}else{
+  print('X is not an Integer');
+}
+
+}
